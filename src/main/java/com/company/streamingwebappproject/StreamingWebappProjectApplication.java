35968da3872cc4ec92fbd454c00de9e08f52c2dc
Mandresy-Andri <andriamasinoromandresy@gmail.com>
2024-02-07 22:21:35 -0500

    cors

--- src/main/java/com/company/streamingwebappproject/StreamingWebappProjectApplication.java
+++ src/main/java/com/company/streamingwebappproject/StreamingWebappProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 
 //@SpringBootApplication()
 //@EnableResourceServer
-@SpringBootApplication()
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class})
 public class StreamingWebappProjectApplication {
 
 	public static void main(String[] args) {

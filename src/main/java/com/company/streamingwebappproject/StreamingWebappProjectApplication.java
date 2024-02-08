9d365fccf14347996227cb1b5ce7ee68cbd4ab4c
Mandresy-Andri <andriamasinoromandresy@gmail.com>
2024-02-08 12:34:47 -0500

    Mock Oauth final

--- src/main/java/com/company/streamingwebappproject/StreamingWebappProjectApplication.java
+++ src/main/java/com/company/streamingwebappproject/StreamingWebappProjectApplication.java
@@ -2,7 +2,6 @@ package com.company.streamingwebappproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication()
